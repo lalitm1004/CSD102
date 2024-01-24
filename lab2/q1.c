719846bda8135aa5c482a3db0caf31c83b4b6863
Lalit M <lalitm1004@gmail.com>
2024-01-24 16:48:44 +0530

    change file strucutre

---
+++ lab2/q1.c
@@ -0,0 +1,36 @@
+/*
+Given and array nums[] of size N having integers in the range [1,N] with
+some of the elements missing. The task is to find the missing elements.
+
+Note: Do it without using extra space and O(n) runtime.
+
+Example 1:
+    Input: n = 4, nums = [1, 2, 2, 4]
+    Output: 3
+
+Example 2:
+    Input: n = 6, nums = [1, 3, 4, 5, 5, 5]
+    Output: 2 6
+*/
+
+#include <stdio.h>
+
+int main() {
+    int nums[100], n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        nums[n+1] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        nums[n + nums[i] - i] = nums[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[n+i] == 0) printf("%d ", i+1);
+    }
+}
